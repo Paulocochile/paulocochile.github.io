7810ae77da7bcd70cb3d9240ed963c29245b0bd2
Paulocochile <63756183+Paulocochile@users.noreply.github.com>
2022-05-20 18:54:31 +0100

    Finished Burger Meny, fully responsive page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,3 @@
-document.getElementById('burger').onclick = function() {
-    // get the clock
-    var myClock = document.getElementById('topmenu');
-
-    // get the current value of the clock's display property
-    var displaySetting = myClock.style.display;
-
-    // now toggle the clock , depending on current state
-    if (displaySetting == 'block') {
-      // clock is visible. hide it
-      myClock.style.display = 'none';
-
-    }
-    else {
-      
-      myClock.style.display = 'block';
-
-    }
-};
-
 function changeMenu() {
     // get the clock
     var myClock = document.getElementById('topmenu');
